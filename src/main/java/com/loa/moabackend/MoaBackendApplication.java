8e62a754e0c957e0e35fea110dceea3985971c87
yyubin <hazing120@gmail.com>
2024-05-27 23:57:17 +0900

    first commit

---
+++ src/main/java/com/loa/moabackend/MoaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.loa.moabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loa/moabackend/MoaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loa.moabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
